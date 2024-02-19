cd5e797b275407760a6670c76bb0e43bac1f0b50
Emil <ismayiloffemil@gmail.com>
2024-02-19 12:18:24 +0400

    my commit

---
+++ src/main/java/com/exampletestprojects/testprojects/TestprojectsApplication.java
@@ -0,0 +1,13 @@
+package com.exampletestprojects.testprojects;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestprojectsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestprojectsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/exampletestprojects/testprojects/TestprojectsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exampletestprojects.testprojects;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestprojectsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
